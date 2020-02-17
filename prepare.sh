12286a7cc13949ac953599dafd94fe20e2324491
Markus Fensterer <markus.fensterer@gmail.com>
2020-02-17 22:54:08 +0100

    create folder for build metadata in prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -13,6 +13,6 @@ fi
 echo "::set-env name=SEMVER_PATCH::$(date +%Y%m%d)"
 
 echo "Generating build metadata"
-mkdir -p ${OS_FLAVOR}/etc/context/metal
+mkdir -p ${OS_FLAVOR}/context/etc/metal
 BUILD_META_FILE="${OS_FLAVOR}/context/etc/metal/build.yaml"
 python -c "import yaml; from datetime import datetime; print yaml.dump(dict(builddate=datetime.now(), commit_ref=\"${BRANCH}\", commit_sha1=\"${GITHUB_SHA}\", gitrepo=\"${GITHUB_REPOSITORY}\"), default_flow_style=False)" | tee -a ${BUILD_META_FILE}
