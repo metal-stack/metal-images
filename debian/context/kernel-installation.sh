0db9da086a41a2aeb9bd13bcd10ba591d05887b6
Markus Fensterer <markus.fensterer@gmail.com>
2020-03-12 15:39:36 +0100

    make purge superfluous to avoid old kernels in hidden image layers

--- debian/context/kernel-installation.sh
+++ debian/context/kernel-installation.sh
@@ -4,9 +4,10 @@ source /etc/os-release
 
 if [ "${ID}" = "ubuntu" ] ; then
     echo "Ubuntu"
+    apt install --yes linux-image-generic
 else
     echo "Debian"
-    apt remove --purge --yes linux-image*
+    # Note: for firewall images the backports kernel is a hard requirements because kernel >= 5.x is necessary for vxlan/evpn
     echo "deb https://deb.debian.org/debian ${VERSION_CODENAME}-backports main" > /etc/apt/sources.list.d/backports.list
     apt update --quiet
     apt install --yes -t buster-backports linux-image-amd64
