28904e11b974ba6cf7fa60aa3a7e618e9955a794
Stefan Majer <stefan.majer@f-i-ts.de>
2020-04-20 14:57:37 +0200

    Hello suricata

---
+++ firewall/context/suricata-installation.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+source /etc/os-release
+
+if [ "${ID}" = "ubuntu" ] ; then
+    echo "Ubuntu - Install suricata from suricata ppa repository"
+    apt-get update --quiet
+    apt-get install --yes --no-install-recommends software-properties-common
+    add-apt-repository --yes ppa:oisf/suricata-stable
+    apt-get update --quiet
+    apt-get install --yes --no-install-recommends suricata
+else
+    echo "Debian - Install suricata from debian repository"
+    apt-get update --quiet
+    apt-get install --yes --no-install-recommends suricata suricata-update
+fi
