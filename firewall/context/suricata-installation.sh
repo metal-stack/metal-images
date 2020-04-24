2b9fcd47360c29b0b0de0556dc45d95887c6c53e
Stefan Majer <stefan.majer@f-i-ts.de>
2020-04-24 13:17:30 +0200

    install suricata in debian from testing repo to get the version on par with ubuntu

--- firewall/context/suricata-installation.sh
+++ firewall/context/suricata-installation.sh
@@ -10,10 +10,12 @@ if [ "${ID}" = "ubuntu" ] ; then
     apt-get update --quiet
     apt-get install --yes --no-install-recommends suricata
 else
-    echo "Debian - Install suricata from debian repository"
+    echo "Debian - Install suricata from debian testing repository"
+    # Note: suricata from main is still on 4.x, 5.x is in testing only
+    echo "deb https://deb.debian.org/debian testing main" > /etc/apt/sources.list.d/testing.list
     rm -f /etc/logrotate.d/suricata
     apt-get update --quiet
-    apt-get install --yes --no-install-recommends suricata suricata-update
+    apt-get install --yes --no-install-recommends -t testing suricata suricata-update
 fi
 
 echo "Enable suricata service"
