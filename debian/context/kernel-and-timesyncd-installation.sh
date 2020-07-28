56643f721419478b2dea20ac8cfa54a653939bf1
Stefan Majer <stefan.majer@f-i-ts.de>
2020-07-28 17:43:53 +0200

    Install openssh-server in debian from backports to have a required fix in combination with nanosleep

--- debian/context/kernel-and-timesyncd-installation.sh
+++ debian/context/kernel-and-timesyncd-installation.sh
@@ -3,14 +3,23 @@ set -e
 source /etc/os-release
 
 if [ "${ID}" = "ubuntu" ] ; then
-    echo "Ubuntu - Install kernel and systemd-timesyncd from ubuntu repository"
-    apt-get install --yes linux-image-generic systemd-timesyncd
+    echo "Ubuntu - Install kernel, openssh-server and systemd-timesyncd from ubuntu repository"
+    apt-get install --yes linux-image-generic openssh-server systemd-timesyncd
 else
-    echo "Debian - Install kernel and systemd-timesyncd from backports repository"
+    echo "Debian - Install kernel, openssh-server and systemd-timesyncd from backports repository"
     # Note: for firewall images the backports kernel is a hard requirements because kernel >= 5.x is necessary for vxlan/evpn
+    # we need openssh-server because of
+    #
+    # openssh (1:8.1p1-5) unstable; urgency=medium
+    # * Apply upstream patches to allow clock_nanosleep() and variants in the
+    #   seccomp sandbox, fixing failures with glibc 2.31
+    # s. https://metadata.ftp-master.debian.org/changelogs/main/o/openssh/testing_changelog
+    #
+    # with ssh to the test vm one gets an audit event for the clock_nanosleep syscall (230)
+    # audit: type=1326 audit(1595317960.526:2): auid=4294967295 uid=107 gid=65534 ses=4294967295 subj=kernel pid=1177 comm="sshd" exe="/usr/sbin/sshd" sig=31 arch=c000003e syscall=230 compat=0 ip=0x7fc40d5eebea code=0x0
     echo "deb https://deb.debian.org/debian ${VERSION_CODENAME}-backports main" > /etc/apt/sources.list.d/backports.list
     apt-get update --quiet
-    apt-get install --yes -t buster-backports linux-image-amd64 systemd-timesyncd
+    apt-get install --yes -t buster-backports linux-image-amd64 openssh-server systemd-timesyncd
 fi
 
 # Remove WIFI, netronome, v4l and liquidio firmware to save ~300MB image size
