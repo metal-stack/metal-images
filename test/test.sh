18b0d69cfee43106d8fb6e9bddbe70eb98ca7e28
Markus Fensterer <markus.fensterer@gmail.com>
2020-07-29 10:11:20 +0200

    added comment for chrony restart in tests

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,7 @@ echo "copy input files and goss"
 scp -o StrictHostKeyChecking=no -i ./key ./inputs/* "root@${IP}":/
 
 echo "do machine test"
+# somehow chrony@vrf104009 needs a double restart to work
 ssh -o StrictHostKeyChecking=no -i ./key "root@${IP}" <<EOF
     MACHINE_TYPE=${MACHINE_TYPE} /prepare.sh
     /install.sh
