1c0d223bf09b24285dcd086e0b75c143660fdd0e
Markus Fensterer <markus.fensterer@gmail.com>
2021-05-17 06:21:11 +0200

    use same detection for cloud-init as done in install.sh

--- debian/cloud-init/cloud-init-custom.sh
+++ debian/cloud-init/cloud-init-custom.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 if [ -e "/etc/metal/userdata" ]; then
-    cloud-init --file /etc/metal/userdata -d single --name write_files
-    cloud-init --file /etc/metal/userdata -d single --name runcmd
-    /var/lib/cloud/instances/iid-datasource-none/scripts/runcmd
+    firstLine=$(sed '1q;d' /etc/metal/userdata)
+    secondLine=$(sed '2q;d' /etc/metal/userdata)
+    if [[ ${firstLine} == "#cloud-config" ]] || [[ "${secondLine}" == "#cloud-config" ]]; then
+        echo "run cloud-init with userdata"
+        cloud-init --file /etc/metal/userdata -d single --name write_files
+        cloud-init --file /etc/metal/userdata -d single --name runcmd
+        /var/lib/cloud/instances/iid-datasource-none/scripts/runcmd
+    fi
 fi
