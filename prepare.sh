88ff045db2776f4db35d3cde666dc6107fad59d8
mreiger <michael@rauschpfeife.net>
2022-01-31 16:36:11 +0100

    Maybe fix order of " \)

--- prepare.sh
+++ prepare.sh
@@ -20,7 +20,7 @@ echo "Generating build metadata"
 mkdir -p "${OS_FLAVOR}/context/etc/metal"
 BUILD_META_FILE="${OS_FLAVOR}/context/etc/metal/build.yaml"
 #python -c "import yaml; from datetime import datetime; print yaml.dump(dict(builddate=datetime.now(), commit_ref=\"${BRANCH}\", commit_sha1=\"${GITHUB_SHA}\", gitrepo=\"${GITHUB_REPOSITORY}\"), default_flow_style=False)" | tee -a "${BUILD_META_FILE}"
-python3 -c "import yaml; from datetime import datetime; print(yaml.dump(dict(builddate=datetime.now(), commit_ref=\"${BRANCH}\", commit_sha1=\"${GITHUB_SHA}\", gitrepo=\"${GITHUB_REPOSITORY}\"), default_flow_style=False)") | tee -a "${BUILD_META_FILE}"
+python3 -c "import yaml; from datetime import datetime; print(yaml.dump(dict(builddate=datetime.now(), commit_ref=\"${BRANCH}\", commit_sha1=\"${GITHUB_SHA}\", gitrepo=\"${GITHUB_REPOSITORY}\"), default_flow_style=False))" | tee -a "${BUILD_META_FILE}"
 
 echo "remove old firecracker images"
 sudo rm -rf /var/lib/firecracker/image/* /var/lib/firecracker/kernel/*
