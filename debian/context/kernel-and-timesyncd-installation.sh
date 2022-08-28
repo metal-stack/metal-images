634ae4fbebc783900ae8a903bdd51129114a68bc
Stefan Majer <stefan.majer@f-i-ts.de>
2022-08-28 10:09:36 +0200

    Debian 11

--- debian/context/kernel-and-timesyncd-installation.sh
+++ debian/context/kernel-and-timesyncd-installation.sh
@@ -20,27 +20,14 @@ if [ "${ID}" = "ubuntu" ] ; then
         /tmp/linux-modules* \
         ${ADDITIONAL_PACKAGES}
 else
-    echo "Debian - Install kernel, openssh-server and systemd-timesyncd from backports repository"
-    # Note: for firewall images the backports kernel is a hard requirements because kernel >= 5.x is necessary for vxlan/evpn
-    # we need openssh-server because of
-    #
-    # openssh (1:8.1p1-5) unstable; urgency=medium
-    # * Apply upstream patches to allow clock_nanosleep() and variants in the
-    #   seccomp sandbox, fixing failures with glibc 2.31
-    # s. https://metadata.ftp-master.debian.org/changelogs/main/o/openssh/testing_changelog
-    #
-    # with ssh to the test vm one gets an audit event for the clock_nanosleep syscall (230)
-    # audit: type=1326 audit(1595317960.526:2): auid=4294967295 uid=107 gid=65534 ses=4294967295 subj=kernel pid=1177 comm="sshd" exe="/usr/sbin/sshd" sig=31 arch=c000003e syscall=230 compat=0 ip=0x7fc40d5eebea code=0x0
-    echo "deb https://deb.debian.org/debian ${VERSION_CODENAME} contrib" > /etc/apt/sources.list.d/contrib.list
-    echo "deb https://deb.debian.org/debian ${VERSION_CODENAME}-backports main contrib non-free" > /etc/apt/sources.list.d/backports.list
-    echo "deb https://deb.debian.org/debian bullseye main contrib non-free" > /etc/apt/sources.list.d/bullseye.list
-    echo "deb https://deb.debian.org/debian-security bullseye-security main contrib non-free" > /etc/apt/sources.list.d/bullseye-security.list
-    apt-get update --quiet
-    apt-get install --yes -t buster-backports ${ADDITIONAL_PACKAGES}
-    # you can get list of installable versions with 
-    # apt list -a linux-image-amd64
-    apt-get install --yes linux-image-amd64="${KERNEL_VERSION}"
-    rm -f /etc/apt/sources.list.d/bullseye.list
+    echo "Debian - Install kernel"
+
+
+    echo "deb http://deb.debian.org/debian bullseye main contrib non-free" > /etc/apt/sources.list.d/contrib-and-nonfree.list
+    echo "deb http://security.debian.org/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list.d/contrib-and-nonfree.list
+    echo "deb http://deb.debian.org/debian bullseye-updates main contrib non-free" >> /etc/apt/sources.list.d/contrib-and-nonfree.list
+
+    apt update && apt install -y intel-microcode linux-image-amd64="${KERNEL_VERSION}"
 fi
 
 # Remove WIFI, netronome, v4l and liquidio firmware to save ~300MB image size
