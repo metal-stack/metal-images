ca605ed38671c479922b61aaeef575c979600aca
Stefan Majer <stefan.majer@f-i-ts.de>
2022-08-31 15:45:05 +0200

    Shellcheck was wrong

--- debian/context/etc/profile.d/serial-console.sh
+++ debian/context/etc/profile.d/serial-console.sh
@@ -2,9 +2,9 @@
 
 resize() {
 	if [[ -t 0 && $# -eq 0 ]]; then
-		local IFS='[;' geometry x y
+		local IFS='[;' escape geometry x y
 		echo -ne '\e7\e[r\e[999;999H\e[6n\e8'
-		read -sd geometry
+		read -sd R escape geometry
 		x=${geometry##*;} y=${geometry%%;*}
 		if [[ ${COLUMNS} -eq ${x} && ${LINES} -eq ${y} ]]; then
 			echo "${TERM} ${x}x${y}"
