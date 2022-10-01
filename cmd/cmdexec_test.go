a19ce754931fca2b07806dade46596cf2d84656d
Gerrit <info@gerritschwerthelm.de>
2022-10-01 13:25:49 +0200

    Introduce cmd mock.

---
+++ cmd/cmdexec_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type fakeexec struct {
+	t         *testing.T
+	mockCount int
+	mocks     []fakeexecparams
+}
+
+type fakeexecparams struct {
+	WantCmd  []string
+	Output   string
+	ExitCode int
+}
+
+func fakeCmd(t *testing.T, params ...fakeexecparams) func(ctx context.Context, command string, args ...string) *exec.Cmd {
+	f := fakeexec{
+		t:     t,
+		mocks: params,
+	}
+	return f.command
+}
+
+func (f *fakeexec) command(ctx context.Context, command string, args ...string) *exec.Cmd {
+	if f.mockCount >= len(f.mocks) {
+		require.Fail(f.t, "more commands called than mocks are available")
+	}
+
+	params := f.mocks[f.mockCount]
+	f.mockCount++
+
+	assert.Equal(f.t, params.WantCmd, append([]string{command}, args...))
+
+	j, err := json.Marshal(params)
+	require.NoError(f.t, err)
+
+	cs := []string{"-test.run=TestHelperProcess", "--", string(j)}
+	cmd := exec.CommandContext(ctx, os.Args[0], cs...) //nolint
+	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
+	return cmd
+}
+
+func TestHelperProcess(t *testing.T) {
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+
+	var f fakeexecparams
+	err := json.Unmarshal([]byte(os.Args[3]), &f)
+	require.NoError(t, err)
+
+	fmt.Fprintf(os.Stdout, f.Output)
+
+	os.Exit(f.ExitCode)
+}
