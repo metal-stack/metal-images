5fd015a60d4fcebf19d5980d04f7e10422b9e08d
Gerrit <info@gerritschwerthelm.de>
2023-07-13 10:02:09 +0200

    Add yaml tags.

--- cmd/v1/build-meta.go
+++ cmd/v1/build-meta.go
@@ -1,11 +1,11 @@
 package v1
 
 type BuildMeta struct {
-	BuildVersion string `json:"buildVersion"`
-	BuildDate    string `json:"buildDate"`
-	BuildSHA     string `json:"buildSHA"`
+	BuildVersion string `json:"buildVersion" yaml:"buildVersion"`
+	BuildDate    string `json:"buildDate" yaml:"buildDate"`
+	BuildSHA     string `json:"buildSHA" yaml:"buildSHA"`
 
-	IgnitionVersion string `json:"ignitionVersion"`
-	GolldpdVersion  string `json:"golldpdVersion"`
-	FrrVersion      string `json:"frrVersion"`
+	IgnitionVersion string `json:"ignitionVersion" yaml:"ignitionVersion"`
+	GolldpdVersion  string `json:"golldpdVersion" yaml:"golldpdVersion"`
+	FrrVersion      string `json:"frrVersion" yaml:"frrVersion"`
 }
