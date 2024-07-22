820a75b0089b7f20f0a6d996d87db1489d980507
Gerrit <info@gerritschwerthelm.de>
2024-07-22 11:27:54 +0200

    Review.

---
+++ almalinux/context/etc/profile.d/serial-console.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+resize() {
+	if [[ -t 0 && $# -eq 0 ]]; then
+		local IFS='[;' escape geometry x y
+		echo -ne '\e7\e[r\e[999;999H\e[6n\e8'
+		read -sd R escape geometry
+		x=${geometry##*;} y=${geometry%%;*}
+		if [[ ${COLUMNS} -eq ${x} && ${LINES} -eq ${y} ]]; then
+			echo "${TERM} ${x}x${y}"
+		else
+			echo "${COLUMNS}x${LINES} -> ${x}x${y}"
+			stty cols ${x} rows ${y}
+		fi
+	else
+		print 'Usage: resize'
+	fi
+}
