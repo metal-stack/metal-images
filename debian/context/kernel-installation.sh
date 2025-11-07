142d8fd8f86f7e830f399bbb4fdcf30a2fdb65a9
Stefan Majer <stefan.majer@f-i-ts.de>
2025-11-07 13:24:07 +0100

    Fix

--- debian/context/kernel-installation.sh
+++ debian/context/kernel-installation.sh
@@ -21,6 +21,12 @@ if [ "${ID}" = "ubuntu" ] ; then
 else
     echo "Debian - Install kernel"
 
+    cat <<EOF > /etc/apt/sources.list
+deb http://deb.debian.org/debian trixie main contrib non-free-firmware
+deb http://deb.debian.org/debian trixie-updates main contrib non-free-firmware
+deb http://security.debian.org/debian-security trixie-security main contrib non-free-firmware
+EOF
+
     apt update && apt install -y intel-microcode linux-image-${KERNEL_VERSION}-amd64 linux-cpupower
 fi
 
